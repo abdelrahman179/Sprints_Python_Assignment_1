7772b30aec532997528b8230c993b4f116fb8e8d
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2021-09-12 00:31:09 +0200

    Update problem10.py

--- problem10.py
+++ problem10.py
@@ -10,15 +10,27 @@
     10- Ask the user for his name then confirm that he has entered his name 
     (not an empty string/integers). then proceed to ask him for his email and print all this data
 '''
-name = input("Please enter your name: ")
-while(name == ""):
-    print("Invalid input")
-    name = input("Please enter your name: ")
+import re
 
-email = input("Please enter your email: ")
-while(email == ""):
-    print("Invalid input")
-    email = input("Please enter your email: ")
 
-print("Your name is: %s\nYour email is: %s" %(name, email))
+def validate_email(email):
+    if (re.search(r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b', email)):
+        print("Your email is "+ email)
+
+    else:
+        print("Invalid Email")
+
+def validate_name(name):
+    regex_name = re.compile(r'^([a-z]+)( [a-z]+)*( [a-z]+)*$',
+                            re.IGNORECASE)
+    if regex_name.search(name):
+        print("Your name is "+ name)
+
+    else:
+        print("Invalid Name")
+
+name = input("Enter Your Name: ")
+email = input("Enter Your Email: ")
+validate_name(name)
+validate_email(email)
     
